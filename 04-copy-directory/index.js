d6c5c3443eacba409a85f493890cd5a56cc34e8c
OlgaKapskaya <olikbuko@gmail.com>
2024-01-22 19:37:56 +0300

    feat: added task 4

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,57 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const folder = path.join(__dirname, 'files');
+const newFolder = path.join(__dirname, 'files-copy');
+
+function copyDirectory(oldFolder, newFolder) {
+  return new Promise((resolve, reject) => {
+    fs.mkdir(newFolder, { recursive: true }, (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      fs.readdir(oldFolder, { withFileTypes: true }, (err, files) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        const copyPromises = files.map((file) =>
+          getNewDirectory(file, oldFolder, newFolder),
+        );
+
+        Promise.all(copyPromises)
+          .then(() => resolve())
+          .catch((err) => reject(err));
+      });
+    });
+  });
+}
+
+function getNewDirectory(file, oldFolder, newFolder) {
+  const sourcePath = path.join(oldFolder, file.name);
+  const targetPath = path.join(newFolder, file.name);
+
+  if (file.isDirectory()) {
+    return copyDirectory(sourcePath, targetPath);
+  } else {
+    return new Promise((resolve, reject) => {
+      const readStream = fs.createReadStream(sourcePath);
+      const writeStream = fs.createWriteStream(targetPath);
+      readStream.on('error', reject);
+      writeStream.on('error', reject);
+      writeStream.on('close', resolve);
+      readStream.pipe(writeStream);
+    });
+  }
+}
+
+copyDirectory(folder, newFolder)
+  .then(() => {
+    console.log(`Success! All files are copied into ${newFolder}`);
+  })
+  .catch((err) => {
+    console.error('Error copying directory:', err);
+  });
